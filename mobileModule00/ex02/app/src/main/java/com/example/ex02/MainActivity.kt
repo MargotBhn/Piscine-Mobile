bd17b6a2820d10da8191daa83f866c698ad747dd
Margot <bihanmargot@gmail.com>
2025-04-10 15:22:49 +0200

    calculator layout done

--- mobileModule00/ex02/app/src/main/java/com/example/ex02/MainActivity.kt
+++ mobileModule00/ex02/app/src/main/java/com/example/ex02/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ex02
 
 
+
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -10,15 +12,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -26,17 +30,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ex02.ui.theme.Ex02Theme
-import kotlin.String
-import kotlin.collections.List
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,13 +59,18 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorLayout () {
-    var calcultorList = listOf(
+    val calculatorList = listOf(
         listOf("7", "8", "9", "C", "AC"),
         listOf("4", "5", "6", "+", "-"),
         listOf("1", "2", "3", "x", "/"),
         listOf("0", ".", "00", "=")
 
         )
+
+    val isPortrait = LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+
+    val displayWeight = if (isPortrait) 0.4f else 0.3f
+    val keypadWeight = if (isPortrait) 0.6f else 0.7f
 
     Scaffold(
         topBar = {
@@ -74,8 +85,8 @@ fun CalculatorLayout () {
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-
-                )
+                ),
+                expandedHeight = 40.dp
 
             )
         }
@@ -90,28 +101,30 @@ fun CalculatorLayout () {
         ) {
             Box(
                 modifier = Modifier
-                    //  .padding(contentPadding)
-                    .background(color = Color.Blue)
-                    .fillMaxWidth(),
+                    // .background(color = Color.Blue)
+                    .fillMaxSize()
+                    .weight(displayWeight),
 
-                contentAlignment = Alignment.CenterEnd
-
+                contentAlignment = Alignment.Center
             ) {
                 CalculatorDisplay()
             }
             Spacer(
                 modifier = Modifier
-                    .weight(1f)
+                    .weight(0.1f)
             )
             Column(
                 modifier = Modifier
-                    //    .padding(contentPadding)
-                    .background(color = Color.Red)
-                    .fillMaxWidth(),
+                    .fillMaxSize()
+                    .weight(keypadWeight)
+                 //   .padding(8.dp)
+                    .background(Color.Green),
+//                verticalArrangement = Arrangement.SpaceEvenly
                 verticalArrangement = Arrangement.Bottom
+
             ) {
 
-                CalculatorKeypad(calcultorList)
+                CalculatorKeypad(calculatorList)
             }
         }
     }
@@ -119,11 +132,20 @@ fun CalculatorLayout () {
 
 @Composable
 fun CalculatorDisplay (){
-    Column()
+    val calculationText by remember { mutableStateOf("0")}
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+           // .background(Color.Red),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center
+
+    )
     {
         Text(
-            text = "0",
-            fontSize = 35.sp
+           text = calculationText,
+            maxLines = 2,
+            fontSize = 25.sp
         )
 
         Text(
@@ -135,30 +157,37 @@ fun CalculatorDisplay (){
 
 @Composable
 fun CalculatorKeypad(
-    calcultorList: List<List<String>>
+    calculatorList: List<List<String>>
 ){
-    calcultorList.forEachIndexed { index, ligne ->
+
+    calculatorList.forEach {line ->
         Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp), // padding entre les boutons,
+           // horizontalArrangement = Arrangement.SpaceEvenly,
+
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 4.dp)
+               // .height(100.dp)
+             //   .padding(4.dp),
+             //   .background(Color.Blue),
+
+                //.padding(horizontal = 3.dp, vertical = 3.dp),// padding sur les bords
+
+
         ) {
-            ligne.forEach { buttonName ->
+            line.forEach { buttonName ->
 
                 if(buttonName == "=") {
                     CalculatorButton(
                         modifier = Modifier
-                            .weight(2f)
-                            .aspectRatio(2f),
+                            .weight(2f),
                         buttonName = buttonName
                     )
                 }
                 else{
                     CalculatorButton(
                         modifier = Modifier
-                            .weight(1f)
-                            .aspectRatio(1f),
+                            .weight(1f),
                         buttonName = buttonName
                     )
                 }
@@ -170,14 +199,30 @@ fun CalculatorKeypad(
 
 @Composable
 fun CalculatorButton (buttonName: String, modifier: Modifier) {
-   androidx.compose.material3.Button(
-       onClick = {Log.d("test","$buttonName was pressed")},
-       modifier = modifier,
-       shape = RoundedCornerShape(20.dp),
-       contentPadding = PaddingValues(10.dp)
-   ) {
-       Text(text = buttonName)
-   }
+    val buttonColor = when (buttonName) {
+        "C", "AC" -> MaterialTheme.colorScheme.surfaceContainer
+        "=" -> MaterialTheme.colorScheme.tertiaryContainer
+        else -> MaterialTheme.colorScheme.secondaryContainer
+    }
+
+    androidx.compose.material3.Button(
+        onClick = { Log.d("test", "$buttonName was pressed") },
+        modifier = modifier
+            .fillMaxWidth(),
+        // shape = RoundedCornerShape(60.dp),
+        shape = CircleShape,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = buttonColor,
+            contentColor = MaterialTheme.colorScheme.secondary
+        ),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 4.dp,
+            pressedElevation = 1.dp
+        )
+    ) {
+        Text(text = buttonName)
+    }
+
 }
 
 
